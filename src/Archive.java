fbfa3f680b25af57b79dc58b362eda613a28a13e
DaniloDuque <dduque@estudiantec.cr>
2023-10-11 12:32:27 -0600

    Cambio de todos los metodos de casilla y tablero a arreglos, verificador en proceso(ya casi esta listo, por favor probarlo)

--- src/Archive.java
+++ src/Archive.java
@@ -1,38 +1,47 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Archive {
 
 
-    private List<Player> data = new ArrayList<>();
+    private HashMap<String, Integer> data;
+
+
 
     public Archive(){
-        data = new ArrayList<>();
-    }
 
-    public void add(Player p){
-        data.add(p);
-    }
-
-    public List<Player> getData(){
-        return data;
-    }
-
-
-    public void addPoints(int i, int points){
-        data.get(i).addPoints(points);
-    }
-
-
-    public void update(List<Player> players){
-
-        for(int i = 0; i < players.size(); i++){
-            data.get(i).setPoints(players.get(i).getPoints());
-        }
+        data = new HashMap<>();
 
     }
 
 
+
+
+
+
+   private void update(String name, int points){
+
+       int aux = data.get(name);
+       if(aux == 0) data.put(name, points);
+       else data.put(name, aux+points);
+
+   }
+
+
+
+   public void update(List<Player> players){
+
+         for(int i = 0; i<players.size(); i++){
+              update(players.get(i).getName(), players.get(i).getPoints());
+         }
+
+   }
+
+
+   public HashMap<String, Integer> getData(){
+       return data;
+   }
 
 
 
