7f8c9b98856f7a45867f9fdedc0865c807161a9c
kyaki <emanuelrs101@gmail.com>
2023-10-15 12:55:11 -0600

    Main menu fuctional

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int pCount = 0;
+
         //Archive datos = new Archive();
         //ResultsGUI r = new ResultsGUI(datos);
+        MainMenu m = new MainMenu();
 
-        Game g = new Game(2);
+        while (pCount == 0){
+            pCount = m.getFin();
+            System.out.println(m.getFin());
+        }
+
+
+        Game g = new Game(pCount);
         g.Turn(true);
-        //g.dispose();
 
 
 
